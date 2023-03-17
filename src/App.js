4a39a7b677bf0b55e1797ed1bca2fe503f15f392
awe8128 <dailywork8128@gmail.com>
2023-03-17 19:06:44 +0900

    exact path added

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
         <>
         <Routes>
-          <Route path='' element={<Header/>}/>
+          <Route exact path='/' element={<Header/>}/>
           <Route path='/About' element={<AboutMePage/>}/>
         </Routes>
         </>
