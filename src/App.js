c48c0765f02914f9f752786a645e94b0bb94618a
awe8128 <dailywork8128@gmail.com>
2024-06-07 18:00:09 +0900

    added new h1 tag

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, {useEffect, useState} from 'react';
-import Header from './components/Header'
-import './App.css';
-import AboutMePage from './pages/Aboutme';
-import { Route,Routes, HashRouter } from 'react-router-dom';
-import Preloader from './components/Preloader';
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
+import "./App.css";
+import AboutMePage from "./pages/Aboutme";
+import { Route, Routes, HashRouter } from "react-router-dom";
+import Preloader from "./components/Preloader";
 
 function App() {
-  const [loading, setloading]=useState(false);
-  useEffect(()=>{
-    setloading(true)
-      setTimeout(()=>{
-          setloading(false);
-      },5000);
-  },[]);
+  const [loading, setloading] = useState(false);
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 5000);
+  }, []);
 
-  
   return (
-    <div className="App"> 
-        <Routes>
-          <Route exact path='/jobhunting' element={<Header/>}></Route>
-          <Route  path='/jobhunting/Portfolio' element={<AboutMePage/>}/>
-        </Routes>
+    <div className="App">
+      <Routes>
+        <Route exact path="/jobhunting" element={<Header />}></Route>
+        <Route path="/jobhunting/Portfolio" element={<AboutMePage />} />
+        <h1>Hi</h1>
+      </Routes>
     </div>
   );
 }
